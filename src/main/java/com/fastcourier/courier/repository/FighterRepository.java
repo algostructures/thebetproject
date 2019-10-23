9a1b3e1099376463cacaf7cfb4a625db29c913a7
Prajapati <sprajapati@ea.com>
2019-10-23 18:03:04 +0530

    adding the services

--- src/main/java/com/fastcourier/courier/repository/FighterRepository.java
+++ src/main/java/com/fastcourier/courier/repository/FighterRepository.java
@@ -5,10 +5,14 @@ import com.fastcourier.courier.model.Fighter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FighterRepository extends JpaRepository<Fighter, Long> {
     @Override
     Optional<Fighter> findById(Long aLong);
+
+    @Override
+    List<Fighter> findAll();
 }
--- src/main/java/com/fastcourier/courier/service/FighterService.java
+++ src/main/java/com/fastcourier/courier/service/FighterService.java
@@ -1,13 +1,24 @@
 package com.fastcourier.courier.service;
 
+import com.fastcourier.courier.model.Fighter;
 import com.fastcourier.courier.repository.FighterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FighterService {
 
     @Autowired
     private FighterRepository fighterRepository;
+
+    public Fighter addFighter(Fighter fighter){
+        return fighterRepository.save(fighter);
+    }
+
+    public List<Fighter> getAllFighters() {
+        return fighterRepository.findAll();
+    }
 
 }
