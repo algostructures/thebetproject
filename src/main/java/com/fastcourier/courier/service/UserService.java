c7042b7b7bd459c7c61b12d33fb9c947fed78363
Saurabh Prajapati <saurabh945644@gmail.com>
2019-10-28 22:17:25 +0530

    adsfgasdf

--- src/main/java/com/fastcourier/courier/service/UserService.java
+++ src/main/java/com/fastcourier/courier/service/UserService.java
@@ -1,8 +1,22 @@
 package com.fastcourier.courier.service;
 
+import com.fastcourier.courier.model.Users;
+import com.fastcourier.courier.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Users addUser(Users user) {
+        return userRepository.save(user);
+    }
+
+    public Users findUserById(Long userId) {
+        return userRepository.findById(userId).get();
+    }
 
 }
